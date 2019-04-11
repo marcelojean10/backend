42aa91d3c99ff796e64d15c23e6d52265fdeccdd
Marcelo Jean <mjean@reweb.com.br>
2019-04-11 10:59:45 -0300

    Ajusta URL heroku

--- src/models/File.js
+++ src/models/File.js
@@ -15,8 +15,8 @@ const File = new mongoose.Schema({
         toJSON: { virtuals: true }
     });
 
-File.virtual('url').get(function () {
-    const url = process.env.PORT || 'http://localhost:3333'
+File.virtual("url").get(function () {
+    const url = process.env.URL || 'http://localhost:3333'
 
     return `${url}/files/${encodeURIComponent(this.path)}`;
 })
